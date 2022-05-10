7181a7e6c7402922f50a7047bde07a4d3375de4e
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-10 21:56:38 +0200

    Routing is finished

---
+++ src/views/errors/error/WrongPath.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const WrongPath = () => {
+    return (
+        <div className={"Error"}>
+            <div className={"WrongPath"}>
+                <h2>WRONG PATH</h2>
+            </div>
+        </div>
+    )
+}
