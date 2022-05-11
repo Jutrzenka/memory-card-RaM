5247b1324f5f0f1e12d0d977c419257a3bc4fc9b
Jutrzenka <kacperczaja1999@gmail.com>
2022-05-11 03:17:56 +0200

    StartView.tsx is finished

--- src/utils/verifyAmount.ts
+++ src/utils/verifyAmount.ts
@@ -6,7 +6,7 @@ export enum Actions {
 export const verifyAmount = (prev:number, action:Actions) => {
     switch (action) {
         case Actions.ADD:
-            if (prev < 8) {
+            if (prev < 10) {
                 return prev + 1
             } else {
                 return prev
